fbf62c343663763f132002723710e6a6b4152520
FabricioSobrinho <fabriciomails3@gmail.com>
2023-07-05 14:31:51 -0400

    ...

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,6 +1,20 @@
+import Message from "../layout/Message"
+import { useLocation } from "react-router-dom"
+
 function Projects() {
+  const location = useLocation()
+  let message = ''
+
+  if (location.state) {
+    message = location.state.message
+  }
+
   return (
-    <div>Projects</div>
+    <div>
+      <h1>Meus projetos</h1>
+      {message && <Message msg={message} type="success"/>}
+      
+    </div>
   )
 }
 
