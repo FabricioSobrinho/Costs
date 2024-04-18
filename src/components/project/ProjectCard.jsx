250308cf54ae0cbbade24abbaa3bfba77361b4e5
FabricioSobrinho <fabriciomails3@gmail.com>
2024-04-18 14:47:50 -0400

    Feat: create categories for show

--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -8,17 +8,36 @@ function ProjectCard({ id, name, budget, category, handleRemove }) {
     e.preventDefault();
     handleRemove(id);
   };
+
+  switch (category) {
+    case 0:
+      category = "infra";
+      break;
+    case 1:
+      category = "desenvolvimento";
+      break;
+    case 2:
+      category = "design";
+      break;
+    case 3:
+      category = "planejamento";
+      break;
+    default:
+      category = "Categoria indefinida"
+      break;
+  }
+
   return (
-    <div className={styles.projectCard}>
+    <div className={styles.projectCard} key={id}>
       <h4> {name}</h4>
       <p>
         <span>Orçamento: </span> R${budget}
       </p>
       <p className={styles.categoryText}>
-        <span className={`${styles[category.toLowerCase()]}`}></span> {category}
+        <span className={`${styles[category]}`}></span> {category}
       </p>
       <div className={styles.projectCardActions}>
-        <Link to={`/projects/${id}`} className={styles.actions}>
+        <Link to={`/projects/${name}`} className={styles.actions}>
           <BsPencil /> Editar
         </Link>
         <button onClick={remove} className={styles.actions}>
